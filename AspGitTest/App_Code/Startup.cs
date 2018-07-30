dcb4be1b11df2f136bf607c9f1890558a67bb4b6
Janet Cardenas <janetcserra@gmail.com>
2018-07-30 14:32:05 -0400

    Add project files.

---
+++ AspGitTest/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspGitTest.Startup))]
+namespace AspGitTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
